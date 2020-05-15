dc607458b7ee71621f447fac6dcf4abdefc7abf8
thomashaile <gthomashaile@gmail.com>
2020-05-15 19:24:35 +0200

    solved

--- 1-promisify-fs/exercise-1-promise.js
+++ 1-promisify-fs/exercise-1-promise.js
@@ -2,6 +2,7 @@
 const fs = require(`fs`);
 const path = require(`path`);
 const assert = require(`assert`);
+const util = require("util");
 
 // declare constants
 const EXERCISE_NAME = path.basename(__filename);
@@ -9,8 +10,8 @@ const START = Date.now();
 
 // declare logging function
 const log = (logId, value) => console.log(
-  `\nlog ${logId} (${Date.now() - START} ms):\n`,
-  value,
+    `\nlog ${logId} (${Date.now() - START} ms):\n`,
+    value,
 );
 
 
@@ -24,29 +25,17 @@ log(1, filePath);
 const newFileContent = process.argv[3];
 log(2, newFileContent);
 
+const readFilePromise = util.promisify(fs.readFile);
+const writeFilePromise = util.promisify(fs.writeFile);
+const appendFileSc = util.promisify(fs.appendFileSync);
 
 log(3, `writing ${fileName} ...`);
-fs.writeFile(filePath, newFileContent, (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  log(4, `reading ${fileName} ...`);
-  fs.readFile(filePath, `utf-8`, (err, fileContent) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    log(5, `asserting ...`);
-    assert.strictEqual(fileContent, newFileContent);
-    log(6, '\033[32mpass!\x1b[0m');
-    fs.appendFileSync(__filename, `\n// pass: ${(new Date()).toLocaleString()}`);
-  });
-
-});
-
-
-
-
+writeFilePromise(filePath, newFileContent).then(() => {
+    log(4, `reading ${fileName} ...`)
+    readFilePromise(filePath, "utf-8").then((fileContent) => {
+        log(5, `asserting ...`);
+        assert.strictEqual(fileContent, newFileContent);
+        log(6, '\033[32mpass!\x1b[0m');
+        appendFileSc(__filename, `\n// pass: ${(new Date()).toLocaleString()}`);
+    }).catch((err) => console.log(err));
+}).catch((err) => console.log(err));
--- 1-promisify-fs/exercise-2-promise.js
+++ 1-promisify-fs/exercise-2-promise.js
@@ -2,6 +2,7 @@
 const fs = require(`fs`);
 const path = require(`path`);
 const assert = require(`assert`);
+const util = require('util');
 
 // declare constants
 const EXERCISE_NAME = path.basename(__filename);
@@ -9,10 +10,9 @@ const START = Date.now();
 
 // declare logging function
 const log = (logId, value) => console.log(
-  `\nlog ${logId} (${Date.now() - START} ms):\n`,
-  value,
+    `\nlog ${logId} (${Date.now() - START} ms):\n`,
+    value,
 );
-
 
 // --- main script ---
 console.log(`\n--- ${EXERCISE_NAME} ---`);
@@ -25,48 +25,26 @@ const fileName2 = process.argv[3];
 const targetPath = path.join(__dirname, fileName2);
 log(2, targetPath);
 
+const readPromise = util.promisify(fs.readFile);
+const copyPromise = util.promisify(fs.copyFile);
+const appendFile = util.promisify(fs.appendFileSync)
+
+
 log(3, `reading original contents from ${fileName1} ...`);
-fs.readFile(sourcePath, `utf-8`, (err, originalSourceContent) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  log(4, `copying to ${fileName2} ...`);
-  fs.copyFile(sourcePath, targetPath, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    log(5, `reading ${fileName1} ...`);
-    fs.readFile(sourcePath, `utf-8`, (err, sourceContent) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      log(6, `asserting ${fileName1} ...`);
-      assert.strictEqual(sourceContent, originalSourceContent);
-
-      log(7, `reading ${fileName2} ...`);
-      fs.readFile(targetPath, `utf-8`, (err, targetContent) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-
-        log(8, `asserting ${fileName2} ...`);
-        assert.strictEqual(targetContent, originalSourceContent);
-
-        log(9, '\033[32mpass!\x1b[0m');
-        fs.appendFileSync(__filename, `\n// pass: ${(new Date()).toLocaleString()}`);
-      });
-    });
-
-  });
-
-});
-
-
-
+readPromise(sourcePath, `utf-8`).then((sourceContent) => {
+    log(4, `copying to ${fileName2} ...`);
+    copyPromise(sourcePath, targetPath).then(() => {
+        log(5, `reading ${fileName1} ...`);
+        readPromise(sourcePath, `utf-8`).then((originalSourceContent) => {
+            log(6, `asserting ${fileName1} ...`);
+            assert.strictEqual(sourceContent, originalSourceContent);
+            log(7, `reading ${fileName2} ...`);
+            readPromise(targetPath, "utf-8").then((targetContent) => {
+                log(8, `asserting ${fileName2} ...`);
+                assert.strictEqual(targetContent, originalSourceContent)
+                log(9, '\033[32mpass!\x1b[0m');
+                appendFile(__filename, `\n// pass: ${(new Date()).toLocaleString()}`);
+            }).catch((err) => console.log(err));
+        }).catch((err) => console.log(err));
+    }).catch((err) => console.log(err))
+}).catch((err) => console.log(err))
